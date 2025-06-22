858c7b62e23996eb3ce5617994b3717d0f4ba6aa
rifqihytam <kambingjawa009@gmail.com>
2025-06-22 13:29:32 +0700

    E-Voting

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -37,8 +37,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
-                Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
                 \App\Filament\Widgets\VotingStatsWidget::class,
                 \App\Filament\Widgets\VotingBarChartWidget::class,
                 \App\Filament\Widgets\CandidatesTable::class,
